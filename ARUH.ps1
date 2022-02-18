4e4a030dcd79eee0c3990dc8204dfd882d26b3a9
Fabrizio Cagnini <Fabrizio.Cagnini@pragroup.it>
2022-02-18 14:39:38 +0100

    Migliorato e abbellito due cose

--- ARUH.ps1
+++ ARUH.ps1
@@ -1,130 +1,64 @@
 
 Clear-Host
 
-Write-Host -ForegroundColor Black -BackgroundColor DarkCyan "`n Fabrizio Cagnini - ARUH.ps1 - Admin Rights User Helper "
-Write-Host -ForegroundColor Black -BackgroundColor DarkGray " ARUH e' in grado di avviare script o eseguibili con i permessi amministrativi da programmi di condivisione schermo senza la possibilità di interagire tramite l'UAC"
+Set-Location $env:USERPROFILE
 
-###################################################################### Logger
-$scriptPath         = split-path -parent $MyInvocation.MyCommand.Definition -ErrorAction SilentlyContinue # Recupero percorso completo dello script
-$scriptName         = $MyInvocation.MyCommand.Name # Recupero nome dello script
-$scriptName         = (Get-Item -Path "$scriptPath\$scriptName" -ErrorAction SilentlyContinue).BaseName # Elaboro il percorso dove verranno salvati i log
-$global:scriptErrs  = @() # Errori / Eccezioni
-$scriptUser         = $env:USERNAME
-$scriptHost         = $env:COMPUTERNAME
+Write-Host -ForegroundColor Black -BackgroundColor Green " Fabrizio Cagnini - ARUH.ps1 - Admin Rights User Helper "
 
-if (-Not(Test-Path -Path "$scriptPath\log\")) {
-    Write-Host -ForegroundColor Yellow "Preparazione di salvataggio output... 0/2`r" -NoNewline
-    New-Item -Path "$scriptPath\" -Name "log" -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
-    Write-Host -ForegroundColor Yellow "Preparazione di salvataggio output... 1/2`r" -NoNewline
-    Start-Sleep -Seconds 1
-    New-Item -Path "$scriptPath\log\" -Name "$scriptName.log" -ItemType File -ErrorAction SilentlyContinue | Out-Null
-    Write-Host -ForegroundColor Yellow "Preparazione di salvataggio output... 2/2`r" -NoNewline
-    Start-Sleep -Seconds 1
-    Write-Host ""
-}
 
-Function Log ($livello, $log)
+Function Log ($livello, $log) # Funzione di Logging
 {
-    try {
-        # Elaboro la stringa di output del log ricevuto
-        $log = "$(Get-Date -Format 'dd-MM-yyyy HH:mm:ss') [$scriptHost] ($scriptUser) $log"
-        
-        if ($livello -match "log") # Informazione 
-        {
-            Write-Host "[i] $log" -ForegroundColor Black -BackgroundColor White
-            if (Test-Path "$scriptPath\log\$scriptName.log") { Add-Content -Path "$scriptPath\log\$scriptName.log" -Value "[i] $log" }
-        } 
-        
-        elseif ($livello -match "err") # Errore
-        {
-            Write-Host "[!] $log" -ForegroundColor Black -BackgroundColor Red
-            if (Test-Path "$scriptPath\log\$scriptName.log") { Add-Content -Path "$scriptPath\log\$scriptName.log" -Value "[!] $log" }
-            $global:scriptErrs += "$log"
-        } 
-        
-        elseif ($livello -match "ok") # Successo
-        {
-            Write-Host "[+] $log" -ForegroundColor Black -BackgroundColor Green
-            if (Test-Path "$scriptPath\log\$scriptName.log") { Add-Content -Path "$scriptPath\log\$scriptName.log" -Value "[+] $log" }
-        } 
-        
-        elseif ($livello -match "warn") # Avviso
-        {
-            Write-Host "[-] $log" -ForegroundColor Black -BackgroundColor Yellow
-            if (Test-Path "$scriptPath\log\$scriptName.log") { Add-Content -Path "$scriptPath\log\$scriptName.log" -Value "[-] $log" }
-        }
-
-        Start-Sleep -Milliseconds 200
-    } catch {
-        Write-Host "[*] La funzione 'Log' ha riscontrato un problema: $($Error[0])" -ForegroundColor Red
-    }
+    # Elaboro la stringa di output del log ricevuto
+    $log = "$(Get-Date -Format 'dd-MM-yyyy HH:mm:ss') $log"
+    
+    if ($livello -match "log") # Informazione 
+    { Write-Host "[i] $log" -ForegroundColor Black -BackgroundColor White } 
+    
+    elseif ($livello -match "err") # Errore
+    { Write-Host "[!] $log" -ForegroundColor Black -BackgroundColor Red } 
+    
+    elseif ($livello -match "ok") # Successo
+    { Write-Host "[+] $log" -ForegroundColor Black -BackgroundColor Green } 
+    
+    elseif ($livello -match "warn") # Avviso
+    { Write-Host "[-] $log" -ForegroundColor Black -BackgroundColor Yellow }
 }
-###################################################################### Logger
 
 
-###################################################################### OpenFileDialog
-Function OpenFileDialog () 
+Function OpenFileDialog ()
 {
     Add-Type -assembly System.Windows.Forms
-    #$PercorsoScr = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath('.\')
-    log "log" "Creo l'oggetto OpenFileDialog..."
     $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
-    $OpenFileDialog.InitialDirectory = $initialDirectory
-    $OpenFileDialog.Multiselect = $false
-    log "log" "Apertura FileDialog..."
-    $response = $OpenFileDialog.ShowDialog() 
-    if ($response -eq "OK" ) { 
-        if (Test-Path -Path ($OpenFileDialog.FileName).Replace('"', "").Replace(" ", "` ")) {
-            log "ok" $OpenFileDialog.FileName
-            return ($OpenFileDialog.FileName).Replace('"', "").Replace(" ", "` ")
-        } else {
-            log "err" "Errore: File non esistente o corrotto"
-            return $false
+    if ($OpenFileDialog.ShowDialog() -match "OK" ) { 
+        if (Test-Path -Path ($OpenFileDialog.FileName).Replace('"', "").Replace(" ", "` ")) { 
+            return ($OpenFileDialog.FileName).Replace('"', "").Replace(" ", "` ") 
+        } else { 
+            log "err" "Percorso dell'eseguibile non valido" 
         }
-    } else {
-        log "err" "Dialog interrotto o annullato"
-        return $false
+    } else { 
+        log "warn" "FileDialog chiuso oppure l'operazione è stata annullata"
+        return 
     }
 }
-###################################################################### OpenFileDialog
 
+# Recupero l'eseguibile da avviare come admin
+$eseguibile = OpenFileDialog
 
-###################################################################### ARUH
-try {
-    Set-Location $env:USERPROFILE
+# Creo il nome dello script
+$script_esecuzione = "ARUH_Script_n$(Get-Random -Minimum 1000 -Maximum 9999).bat"
 
-    $eseguibile = OpenFileDialog
-    if (-Not($eseguibile)) {
-        log "err" "Annullato"
-        return $false
-    }
+# Creo lo script di esecuzione
+New-Item -Path "C:\Windows\Temp\" -Name "$script_esecuzione" -Value '@echo off && start "" "$" && pause'.Replace("$", $eseguibile) -Force -ErrorAction Stop
 
-    log("Creazione script di esecuzione...")
-    $sn = "ARUH_Script_n$(Get-Random -Minimum 1000 -Maximum 9999).bat"
-    New-Item -Path "C:\Windows\Temp\" -Name "$sn" -Value '@echo off && start "" "$" && pause'.Replace("$", $eseguibile) -Force | Out-Null
-    if (Test-Path -Path "C:\Windows\Temp\$sn") {
-        log "ok" "Script di esecuzione creato"
+# Aggiorno il terminale printando il contenuto dello script
+Write-Host -BackgroundColor DarkGray -ForegroundColor Black "`nScript di esecuzione: $(Get-Content "C:\Windows\Temp\$script_esecuzione")`n"
 
-        Write-Host -BackgroundColor DarkGray -ForegroundColor Black "`nScript di esecuzione: $(Get-Content "C:\Windows\Temp\$sn")`n"
+# Verifico il contenuto dello script
+if (-Not((Get-Content -Path "C:\Windows\Temp\$script_esecuzione") -like "*$eseguibile*"))
+{ log "err" "Script di esecuzione non corretto!"; return }
 
-        if ((Get-Content -Path "C:\Windows\Temp\$sn") -like "*$eseguibile*") {
-            log "ok" "Script di esecuzione corretto"
-        } 
-        else {
-            log "err" "Script di esecuzione risultato incorretto"
-            return $false
-        }
-    } 
-    else {
-        log "err" "Script di esecuzione non creato"
-        return $false
-    }
+# Recupero le credenziali amministrative
+$credenziali = Get-Credential -UserName "<DOMAIN>\<Admin Username>" -Message "Per avviare il processo di esecuzione con diritti amministrativi è necessario inserire le corrette credenziali privilegiate." -ErrorAction Stop
 
-    $credenziali = Get-Credential -UserName "<DOMAIN>\<Admin Username>" -Message "Per avviare il processo di esecuzione con diritti amministrativi è necessario inserire le corrette credenziali privilegiate."
-    Start-Process Powershell.exe -Credential $credenziali -ArgumentList "-noprofile -command &{Start-Process Powershell -ArgumentList C:\Windows\Temp\$sn -verb runas}"
-} 
-Catch {
-    log "err" $Error[0]
-    return $false
-}
-###################################################################### ARUH
+# Avvio il processo powershell
+Start-Process Powershell.exe -Credential $credenziali -ArgumentList "-noprofile -command &{Start-Process Powershell -ArgumentList C:\Windows\Temp\$script_esecuzione -verb runas}"
--- README.md
+++ README.md
@@ -1,39 +1,12 @@
-# Admin Rights User Helper
-Questo scrippettino PowerShell permette di avviare eseguibili con permessi di amministratore durante il supporto tecnico remoto attraverso Teams o altri programmi che non consentono di interfacciarsi con l'UAC di Windows durante la condivisione dello schermo.
-
-Non puoi capire quanto mi abbia aiutato questo script!
-Ricordo ancora i silenzi imbarazzanti dopo aver detto all'utente:
-> "Hey... tu che stai tra la sedia e il monitor... ho pessime notizie per te, non posso aiutarti perché l'altro programma di controllo remoto non funge".
-
-Ricorda sempre di non dare la colpa ai sistemi informatici, ma ad altre persone. (?!?!)
+# Admin Right User Helpder
+Script creato come workaround quando a noi dell'IT non funziona il software di controllo remoto che usiamo per interagire con l'UAC dell'utente.
+C'è poco da fare quando non va quel dannato programma di controllo remoto, o quando la connessione scarseggia, oppure quando l'UAC blocca software di condivisione del contenuto come Teams o altri... In quei momenti le madonne scendono dal cielo. Ma come sempre, trovo la soluzione scripteggiando!
 
 ## Guida
-Se anche tu come me hai bisogno di dormire e di urlare allo stesso tempo, leggi questa guida, ti calmerà durante la sessione di troubleshooting remota.
-
-Ti capiterà di chiamare, o esser chiamato da un utente che ha bisogno di installare qualche programma per un progetto mai sentito di cui ti importa poco (sii sincero) e so che la prima cosa che gli chiederai sarà:
-> "E' stato aperto un ticket, approvato dal tuo capo e dalla security?" 
-
-E ti toccherai i maroni per speranza che ti risponda "No" così da continuare a far finta di lavorare scansafatiche bastardo. 
-
-Ecco, sta volta ti ha risposto "Si", menasfighe.
-
-Quindi gli chiederai l'hostname della sua macchina per collegarti al suo computer usando il programmino che ti permetterà di avviare l'installer come admin ma... non funziona. 
-E nemmeno dalla condivisione schermo di Teams potrai farlo, perché non ti permetterà di interagire con l'UAC di Windows.
-
-Ma ho trovato la soluzione al problema (non ai miei mentali lol), fatti condividere lo schermo via Teams e copiagli ARUH.ps1 in locale.
-1. Apri un CMD e recati nella cartella dove hai copiato ARUH.ps1.
-2. Avvialo con il comando: 
-```
-powershell.exe -ExecutionPolicy Bypass -File ARUH.ps1
-```
-3. Si avvierà così lo script che ti chiederà di scegliere un file, dovrai selezionare l'installer del programma da installare all'utente.
-Lo script effettuerà delle verifiche e scriverà un piccolo file .bat nella cartella:
-> C:\Windows\Temp\<file_id>.bat
-4. Lo script ti chiederà la tua utenza amministrativa (scrivi solo il tuo username amministrativo)
-5. Controlla che il dominio che precede il tuo username admin sia corretto e inserisci la tua password amministrativa
-6. Pigia ENTER
-7. Lo script avvierà dal computer dell'utente quel .bat citato prima e chiederà all'utente di aprire la powershell come amministratore ma tu non lo vedrai. Per favore, digli di cliccare su "Si" prima che mi arrabbi.
-
-Ecco fatto! Avrai avviato il tuo eseguibile come amministratore usando la condivisione schermo da Teams. 50€ grazie.
-
-
+1. Fatti condividere lo schermo dall'utente e copiagli in locale ARUH.ps1
+2. Tasto destro su ARUH.ps1 e selezione "Esegui con Powershell"
+3. Scegli l'eseguibile da lanciare
+4. Inserisci le credenziali con il dominio che precede il tuo user admin
+5. Pigia la password corretta
+6. Premi ENTER! 'ENTER ESCHER ENTER ESCHER. Eeeh spaccalo!' Cit.
+7. Fine
